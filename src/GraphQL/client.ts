e2e78d7d43fb0b9bd188af432a2bd47ffeacaeaf
xoma_star <sasha.sirotyin09@gmail.com>
2022-03-15 21:54:01 +0500

    a

--- src/GraphQL/client.ts
+++ src/GraphQL/client.ts
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache} from "@apollo/client";
 import {GRAPHQL, LOCALHOST_SERVER_URL} from "../Constants";
 
 const client = new ApolloClient({
-    uri: (process.env.GRAPHQL_SERVER_URL || LOCALHOST_SERVER_URL) + GRAPHQL,
+    uri: (process.env.REACT_APP_GRAPHQL_SERVER_URL || LOCALHOST_SERVER_URL) + GRAPHQL,
     cache: new InMemoryCache()
 })
 
