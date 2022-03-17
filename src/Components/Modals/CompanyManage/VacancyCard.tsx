ee35bb7a94e14c6c122a688867fd2efdeb07082e
xoma_star <sasha.sirotyin09@gmail.com>
2022-03-17 20:32:56 +0500

    vk bridge taptic test

--- src/Components/Modals/CompanyManage/VacancyCard.tsx
+++ src/Components/Modals/CompanyManage/VacancyCard.tsx
@@ -1,7 +1,8 @@
-import {Button, Checkbox, FormItem, FormLayout, Input, ModalCard, Slider} from "@vkontakte/vkui";
-import {Icon56UsersOutline} from "@vkontakte/icons";
-import {useState} from "react";
+import {Avatar, Button, Cell, Checkbox, FormItem, FormLayout, Input, Link, ModalCard, Slider} from "@vkontakte/vkui";
+import {Icon28OnOffOutline, Icon28SettingsOutline, Icon28WheelOutline, Icon56UsersOutline} from "@vkontakte/icons";
+import React, {useState} from "react";
 import bridge from "@vkontakte/vk-bridge";
+import useDebounce from "../../../Hooks/useDebounce";
 
 interface props{
     id: string
@@ -9,6 +10,8 @@ interface props{
 
 export const WorkersVacancyCard = ({id}: props) => {
     const [vac, setVac] = useState(0)
+    const debounced = useDebounce(vac, 500)
+
     const hapticHandler = async (e: number) => {
         bridge.send('VKWebAppTapticImpactOccurred', {style: 'light'})
         setVac(e)
@@ -17,7 +20,7 @@ export const WorkersVacancyCard = ({id}: props) => {
         header={'Вакансии'}
         id={id}
         icon={<Icon56UsersOutline/>}
-        actions={<Button size={'l'}>Готово</Button>}
+        actions={<Button loading={debounced !== vac} disabled={debounced !== vac} size={'l'}>Готово</Button>}
     >
         <FormLayout>
             <FormItem top={'Открытые вакансии'}>
@@ -29,6 +32,14 @@ export const WorkersVacancyCard = ({id}: props) => {
             <FormItem>
                 <Checkbox>Набирать с высшим образованием</Checkbox>
             </FormItem>
+            <Cell before={<Avatar>{
+                debounced ? <Icon28WheelOutline width={40} height={40} className={'spinningAccent'}/> :
+                    <Icon28OnOffOutline style={{color: 'var(--dynamic_red)'}}/>
+            }</Avatar>}
+                description={<Link>Подробнее</Link>}
+            >
+                {debounced ? 'Ждем отклика от рабочих' : 'Набор не ведется'}
+            </Cell>
         </FormLayout>
     </ModalCard>
 }
