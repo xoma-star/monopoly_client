8f8cde446dbea497e8a299231d593cc76f4ca93d
xoma_star <sasha.sirotyin09@gmail.com>
2022-03-20 21:01:46 +0500

    history API android

--- src/Redux/ActionCreators/vkui.ts
+++ src/Redux/ActionCreators/vkui.ts
@@ -31,6 +31,7 @@ export const VKUIhistoryPush = (p: VKUIhistory) => {
 
 export const VKUIHistoryBack = () => {
     return (dispatch: Dispatch<VKUIAction>) => {
+        window.history.back()
         dispatch({type: VKUIActionTypes.HISTORY_BACK})
     }
 }
@@ -38,6 +39,7 @@ export const VKUIHistoryBack = () => {
 export const VKUIModalSet = (p: null | VKUIModals) => {
     return (dispatch: Dispatch<VKUIAction>) => {
         window.history.pushState({}, '')
+        if(p === null) window.history.back()
         dispatch({type: VKUIActionTypes.SET_MODAL, payload: p})
     }
 }
