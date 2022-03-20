25d9507574e80698fe7fe364ab9e0bf792bb5bac
xoma_star <sasha.sirotyin09@gmail.com>
2022-03-20 21:10:00 +0500

    history API android

--- src/Components/Panels/Main/index.tsx
+++ src/Components/Panels/Main/index.tsx
@@ -3,10 +3,14 @@ import {WelcomeRow} from "./WelcomeRow";
 import {FastActionsRow} from "./FastActionsRow";
 import {NewsBlock} from "../../Common/Blocks/NewsBlock";
 import {CreateCompanyBottomButton} from "../../Common/Blocks/CreateCompanyBottomButton";
+import {useTypedSelector} from "../../../Hooks/useTypedSelector";
 
 export const MainPanel = () => {
+    const VKUI = useTypedSelector(s => s.vkui)
     return <React.Fragment>
         <WelcomeRow/>
+        {VKUI.history.map(v => `${v.view} ${v.panel}\n`)}
+        {VKUI.modal.map(v => `${v}\n`)}
         <FastActionsRow/>
         <NewsBlock style={{marginTop: 40}} count={1} showNext/>
         <CreateCompanyBottomButton/>
